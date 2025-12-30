882801ed04618946167c2e09c929fa815fa66621
FurkanSadik <furkan4320@gmail.com>
2025-12-30 17:45:11 +0300

    feat: haptics ekranında görsel ve sistem titreşimi (Vibration) ile destek sağlandı

--- screens/HapticsScreen.js
+++ screens/HapticsScreen.js
@@ -1,29 +1,41 @@
-import { View, Button, StyleSheet } from 'react-native';
+import { View, Button, StyleSheet, Alert, Vibration } from 'react-native';
 import * as Haptics from 'expo-haptics';
 
 export default function HapticsScreen() {
   const impactLight = () => {
     Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    Vibration.vibrate(50);
+    Alert.alert('Haptics', 'Hafif darbe titreşimi denendi.');
   };
 
   const impactMedium = () => {
     Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+    Vibration.vibrate(100);
+    Alert.alert('Haptics', 'Orta darbe titreşimi denendi.');
   };
 
   const impactHeavy = () => {
     Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+    Vibration.vibrate(200);
+    Alert.alert('Haptics', 'Güçlü darbe titreşimi denendi.');
   };
 
   const selection = () => {
     Haptics.selectionAsync();
+    Vibration.vibrate(60);
+    Alert.alert('Haptics', 'Seçim hissi titreşimi denendi.');
   };
 
   const success = () => {
     Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+    Vibration.vibrate([0, 80, 60, 80]);
+    Alert.alert('Haptics', 'Başarılı bildirim titreşimi denendi.');
   };
 
   const error = () => {
     Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+    Vibration.vibrate([0, 150, 80, 150]);
+    Alert.alert('Haptics', 'Hata bildirim titreşimi denendi.');
   };
 
   return (
